ec1e8a42ba8c2123fab674a3e98b4cf6b7c8dd16
Adam Sitnik <adam.sitnik@gmail.com>
2022-07-14 09:39:27 +0200

    push test code the repo so I can quickly clone it from various VMs

--- Program.cs
+++ Program.cs
@@ -33,6 +33,26 @@ namespace ReadKey
 
         static void Main()
         {
+            while (true)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                Console.WriteLine($"Key: {cki.Key} Modifiers: {cki.Modifiers} KeyChar: '{(int)cki.KeyChar}' '{Print(cki.KeyChar)}'");
+            }
+
+            static string Print(char keyChar)
+                => keyChar switch
+                {
+                    '\b' => "Backspace",
+                    '\r' => "CR",
+                    '\n' => "NL",
+                    '\t' => "Tab",
+                    (char)27 => "Escape",
+                    _ => keyChar.ToString()
+                };
+        }
+
+        static void Record()
+        {
             byte verase = ConfigureTerminal();
 
             (string term, string actualPath, byte[] db) = ReadTerminalSettings();
